f321817dec71b12afd4c8af7da26d45a77754aaa
mehrnoosh <mehr910093@gmail.com>
2022-08-01 15:34:59 +0430

    Complete the structure of todo list

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
-function TodoForm({onSubmit}) {
+function TodoForm({onSubmit, edit}) {
   const [input, setInput] = useState('');
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  })
   
   const handleChange = e => {
     setInput(e.target.value);
@@ -10,16 +15,18 @@ function TodoForm({onSubmit}) {
   const handleSubmit = e => {
     e.preventDefault();
 
-    // onSubmit({
-    //   id: Math.floor(Math.random * 10000),
-    //   text: input
-    // });
+    onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+      isComplete:false
+    });
   }
 
   return (
     <form className='todo-form' onSubmit={handleSubmit}>
       <input
         className='todo-input'
+        ref={inputRef}
         type="text"
         name="text"
         placeholder='Add a todo'
